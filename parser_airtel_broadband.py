b1c13de8837b15d6ee7c61b3e111aa1f8996cc49
Nikhil <nikhilsv92@gmail.com>
2014-02-09 12:28:50 +0530

    parser_airtel_broadband.py: basic parsing and getting data based on column number added

---
+++ parser_airtel_broadband.py
@@ -0,0 +1,30 @@
+import csv
+import sys
+import re
+
+# Steps
+#   - Dates are there in 2nd and 3rd col
+#   - Days are in 1st column
+#   - Data used in 10KB is in 9th column
+
+input_file='/Users/nsonti/github_projects/airtel_broadband_data_parser/airtel_usage_feb_9'
+
+def get_data_from_column(file_name,delimiter,*col_no):
+    fh = open(file_name,'rb')
+    col = []
+    for line in csv.reader(fh,delimiter=' ',skipinitialspace=True):
+        store_string=""
+        for cols in col_no:
+            store_string=store_string+" "+str(line[cols])
+        col.append(store_string)
+
+    return col
+
+def main():
+    global input_file
+    print '1st'
+    print str(get_data_from_column(input_file,' ',9))
+    print '2nd'
+    print str(get_data_from_column(input_file,' ',1,2))
+
+main()
