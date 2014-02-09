f95719b7df62e4ba78fae4283851be27766054af
Nikhil <nikhilsv92@gmail.com>
2014-02-09 12:33:48 +0530

    parser_airtel_broadband.py: extra space while returning column corrected

--- parser_airtel_broadband.py
+++ parser_airtel_broadband.py
@@ -15,10 +15,14 @@ def get_data_from_column(file_name,delimiter,*col_no):
     for line in csv.reader(fh,delimiter=' ',skipinitialspace=True):
         store_string=""
         for cols in col_no:
-            store_string=store_string+" "+str(line[cols])
-        col.append(store_string)
+            store_string=store_string+str(line[cols])+" "
+        tmp = store_string.rstrip()
+        col.append(tmp)
 
     return col
+
+def get_output_dic_day_based():
+    return
 
 def main():
     global input_file
@@ -26,5 +30,7 @@ def main():
     print str(get_data_from_column(input_file,' ',9))
     print '2nd'
     print str(get_data_from_column(input_file,' ',1,2))
+    print '3rd'
+    print str(get_data_from_column(input_file,' ',0))
 
 main()
